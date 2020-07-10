d51d4bb07180fb3b3e73db8ca7292d269e29b1a8
Jérémy Guérin-Coquereau <jeremy@lifeaz.fr>
2020-07-10 16:11:10 +0200

    Merge(home)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { QuizComponent } from './quiz/quiz.component';
+import { QuizComponent } from './pages/quiz/quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
     QuizComponent
   ],
   imports: [
