1b4451783023c609e84fa00d0c70cdb8f06a1fab
Jérémy Guérin-Coquereau <jeremy@lifeaz.fr>
2020-07-10 16:11:02 +0200

    merge(home)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent
+    QuizComponent
   ],
   imports: [
     BrowserModule,
